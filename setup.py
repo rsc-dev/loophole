3d1ab1be766e272b6facdb55dd456d2a37ca05ce
rsc-dev <roscoe@o2.pl>
2017-10-08 10:12:38 +0200

    Added pypi setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'loophole',
+  packages = ['loophole'],
+  version = '05',
+  description = 'Polar devices Python API and CLI.',
+  author = 'Radoslaw Matusiak',
+  author_email = 'radoslaw.matusiak@gmail.com',
+  url = 'https://github.com/rsc-dev/loophole',
+  download_url = 'https://github.com/rsc-dev/loophole/releases/tag/0.5',
+  keywords = ['polar', 'api', 'cli', 'reverse', ''],
+  classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+  ],
+)
